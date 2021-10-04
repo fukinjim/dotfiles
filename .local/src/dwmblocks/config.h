057a08c0a7054d253faa49b22a28dfac52f1e186
fukinjim <fukin.jim@gmail.com>
2021-10-03 22:43:49 -0700

    update

---
+++ .local/src/dwmblocks/config.h
@@ -0,0 +1,21 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*{"",	"sb-news",		0,	6},*/
+	/* {"",	"sb-memory",	10,	14}, */
+	/* {"",	"sb-cpu",		10,	18}, */
+	/*{"",	"sb-forecast",	18000,	5},*/
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-music",	0,	11},
+	{"",	"sb-clock",	60,	1},
+	{"",	"sb-internet",	5,	4},
+	{"",	"battery.sh",	5,	3},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " ";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
